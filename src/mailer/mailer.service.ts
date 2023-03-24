6524de07bb5b6d4f4aabcaaed6ba1cf9b7b6d9db
Kakeshin <kakeshin.engineer@gmail.com>
2023-03-24 19:56:21 +0900

    fix

--- src/mailer/mailer.service.ts
+++ src/mailer/mailer.service.ts
@@ -5,7 +5,7 @@ import { Mailer } from './mailer.interface';
 @Injectable()
 export default class MailerService {
   async getHello(mailer: Mailer): Promise<string> {
-    console.log('Service Start', mailer.token, process.env.TOKEN);
+    console.log(typeof mailer.token, typeof process.env.TOKEN);
     if (`${mailer.token}` !== `${process.env.TOKEN}`) {
       console.error('Token Error');
       return 'Bad Send';
